222654e2330bdcaa229373c537bb96ca1309fbca
Alexei Naraev <marlausor@gmail.com>
2024-04-07 21:18:30 +0300

    Changed link that was used to get infromation from weather api (Now it returnes information for hours. Not for days). Also changed weather api json parser according to new link

--- weather.py
+++ weather.py
@@ -12,9 +12,14 @@ class RequestWeather():
     async def get_weather_raw(self, model: WeatherInputModel) -> dict | str:
         try:
             async with httpx.AsyncClient() as client:
-                ans = await client.get(f"https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline/{model.city}/{model.date_from}/{model.date_to}?key={self.token}&unitGroup=metric&include=days&elements=datetime,tempmax,tempmin,temp")
+                url_to_request = f"https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline/{model.city}/{model.date_from}/{model.date_to}?key={self.token}&unitGroup=metric&include=hours&elements=datetime,temp"
+                ans = await client.get(url_to_request)
             decoded_answer = ans.content.decode()
             out = json.loads(decoded_answer)
+        except httpx.ConnectTimeout as e:
+            logging.error("Whoops. Something went wrong while requesting weather information.")
+            logging.error(e.args)
+            out = {"error": e.args}
         except httpx.ConnectError as e:
             logging.error("Whoops. Something went wrong while requesting weather information.")
             logging.error(e.args)
@@ -29,17 +34,19 @@ class RequestWeather():
     async def get_weather_json(self, model: WeatherInputModel) -> dict:
         ans = await self.get_weather_raw(model)
         try:
-            temperatures = [a["temp"] for a in ans["days"]]
-            min_temperatures = [a["tempmin"] for a in ans["days"]]
-            max_temperatures = [a["tempmax"] for a in ans["days"]]
+            temperatures = sorted(a["temp"] for b in ans["days"] for a in b["hours"])
+            min_temperature = temperatures[0]
+            max_temperature = temperatures[-1]
+            arithmetic_temperature = round(sum(temperatures) / len(temperatures), 1)
+            median_temperature = temperatures[len(temperatures) // 2]
             return {
                 "service": "weather",
                 "data": {
                     "temperature_c": {
-                        "average": round(sum(temperatures) / len(temperatures), 2),
-                        "median": temperatures[len(temperatures) // 2],
-                        "min": min(min_temperatures),
-                        "max": max(max_temperatures)
+                        "average": arithmetic_temperature,
+                        "median": median_temperature,
+                        "min": min_temperature,
+                        "max": max_temperature
                     }
                 }
             }
