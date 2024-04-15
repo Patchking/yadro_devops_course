0cf17b82aa3fbab5f28a0b9a91f04c57c325e7fc
Alexei Naraev <marlausor@gmail.com>
2024-04-15 21:46:31 +0300

    Коммит для проверки пайплайна. Пытаюсь сделать так, чтобы питом импортировал пакеты относительно основной директории. У меня все работает, пайплайн фейлиться. Это временный коммит с целью посмотреть что происходит внутри контейнера

--- config.py
+++ config.py
@@ -1,4 +1,5 @@
 import os
+import sys
 import logging
 import bestconfig
 
@@ -11,10 +12,14 @@ config = bestconfig.Config()
 logger = logging.Logger("base_logger")
 
 # Настройка PYTHONPATH
+
+sys.path.append(os.getcwd())
 if "PYTHONPATH" not in os.environ:
     os.environ["PYTHONPATH"] = ""
 if os.getcwd() not in os.environ["PYTHONPATH"].split(":"):
     os.environ["PYTHONPATH"] += f":{os.getcwd()}"
+
+print(os.environ)
 
 FASTAPI_PORT = config.get("PORT", default_value=8000)
 WEATHER_TOKEN = config.get("API_KEY", default_value=None)
